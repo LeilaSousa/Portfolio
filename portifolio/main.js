2ed3101e563f366e5bd6a5e5d632f032b0f859ca
LeilaSousa <leilacb0@hotmail.com>
2022-09-14 14:56:58 -0300

    criei portifolio

---
+++ portifolio/main.js
@@ -0,0 +1,10 @@
+let burguer = document.getElementById('burguer')
+let nav = document.getElementById('nav')
+
+function menu(){
+    if(nav.style.display == 'block') 
+    {nav.style.display = 'none'
+    }else{
+        nav.style.display = 'block'
+    }
+}
